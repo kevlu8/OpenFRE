c65feb5e9d4b7df91cebcda2abec823ac8f002a4
ExplodingBottle <72459125+ExplodingBottle@users.noreply.github.com>
2022-02-19 13:04:51 +0100

    Quick resource fix

--- OpenFRE.dll/resource.h
+++ OpenFRE.dll/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by OpenFRE.dll.rc
+// Used by Resources.rc
 
 // Valeurs par défaut suivantes des nouveaux objets
 // 
